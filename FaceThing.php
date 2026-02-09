660f188fe41f3bf1953a1b3b336e0cb9dec0937d
Alan Osys <72283731+AlanOsys@users.noreply.github.com>
2026-02-09 08:33:43 +0000

    Add files via upload

---
+++ FaceThing.php
@@ -0,0 +1,11 @@
+// Source - https://stackoverflow.com/a/56315478
+// Posted by Integraty_dev, modified by community. See post 'Timeline' for change history
+// Retrieved 2026-02-09, License - CC BY-SA 4.0
+
+<html>
+<head>
+<title>Run my Python files</title>
+<?PHP
+echo shell_exec("python test.py 'parameter1'");
+?>
+</head>
